810da486a6621b9e85615e97711e2a405edd199c
Hejun Chen <hejunchen@gmail.com>
2015-03-23 15:57:02 -0700

    made jsonp working

--- js/services/BBP_Services.js
+++ js/services/BBP_Services.js
@@ -4,18 +4,15 @@
 
 app.service('ApiJsonDataAccessService', ['$http','$q',function($http,$q){
 
-    //Due to the 'Access-Control-Allow-Origin' restriction, unable to launch this application outside of www.bestbuy.ca domain
-    //had to extract all json data and save them into individual .json file.
-
-    var GetAllCategories = function(){
+      var GetAllCategories = function(){
 
         var deferred = $q.defer();
         var categories = [];
-        var url;// = "http://www.bestbuy.ca/api/v2/json/category/Departments?callback=JSON_CALLBACK";
-        url = 'json/AllCategories.json';
+        var url = "http://www.bestbuy.ca/api/v2/json/category/Departments?callback=?";
+        //url = 'json/AllCategories.json';
         console.log('URL: ' + url.toString());
 
-        $http.get(url)
+        $.getJSON(url)
             .success(function(data){
                 categories = data;
                 deferred.resolve(categories);
@@ -28,27 +25,27 @@ app.service('ApiJsonDataAccessService', ['$http','$q',function($http,$q){
         return deferred.promise;
 
 
-    }
+    };
 
     var GetProductsByCategoryId = function(categoryId){
         var deferred = $q.defer();
         var param = '';
-        if (categoryId == null)
-            param = 'departments.json';
+        if (categoryId === null)
+            param = 'All';
         else
-            param = categoryId + '.json';
+            param = categoryId.toString();
 
         var products = [];
-        var url; // = "http://www.bestbuy.ca/api/v2/json/search?categoryid=" + param;
-        url = 'json/' + param;
+        var url = "http://www.bestbuy.ca/api/v2/json/search?categoryid=" + param + '&callback=?';
+        //url = 'json/' + param;
         console.log('URL: ' + url.toString());
-        $http.get(url)
+        $.getJSON(url)
             .success(function(data){
                 products = data;
                 deferred.resolve(products);
             });
         return deferred.promise;
-    }
+    };
 
     //var GetProductDetailsBySku = function(sku){
     //    var deferred = $q.defer();
